654785b10b6687a5c750b19790d708a3a89f58b3
Liza <Lizka.givka@mail.ru>
2023-09-22 13:59:21 +0300

    Add new file

---
+++ Task8.java
@@ -0,0 +1,9 @@
+public class Task8{
+	public static void main(String[] args) {
+		int s = 0;
+		for(int i = 0; i < args.length; i++) {
+				s = s + Integer.parseInt(args[i]);
+		}
+	    System.out.println(s);
+	}
+}
