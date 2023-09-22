d873b1df33c4aa631e3a0030e56d003e238bd0d6
Liza <Lizka.givka@mail.ru>
2023-09-22 13:56:03 +0300

    Add new file

---
+++ Task2.java
@@ -0,0 +1,8 @@
+public class Task2 {
+     public static void main(String[] args) {
+	 int a=Integer.parseInt(args[0]);
+	 int b=Integer.parseInt(args[1]);
+	 if (b>a){ System.out.println(b);
+	 } else { System.out.println(a); }
+	 }
+}
